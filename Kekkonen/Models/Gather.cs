49f4005833fff6aa7db1975519f765a9857a2fe5
pahasalami <salami@deadbeef.sexy>
2018-11-22 00:14:22 +0100

    Gather: Added files and implemented basic models

---
+++ Kekkonen/Models/Gather.cs
@@ -0,0 +1,53 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kekkonen.Models
+{
+
+    public class GatherGame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<GatherTeam> Teams { get; set; }
+        public GatherTeam WinningTeam { get; set; }
+        public int MaxTeams { get; set; }
+
+        public static List<string> FallbackTeamNames = new List<string>()
+        {
+            "Red", "Blue", "Yellow", "Green", "Cyan", "Pink"
+        };
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Teams: {string.Join(", ", Teams)}/{MaxTeams}";
+        }
+    }
+
+    public class GatherTeam
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<GatherPlayer> Players { get; set; }
+        public int MaxPlayers { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Players: {string.Join(", ", Players)}/{MaxPlayers}";
+        }
+
+        public bool IsFull
+        {
+            get
+            {
+                return Players.Count >= MaxPlayers;
+            }
+        }
+    }
+
+    public class GatherPlayer
+    {
+        public IGuildUser Player { get; set; }
+    }
+}
---
+++ Kekkonen/Modules/GatherModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kekkonen.Modules
+{
+    public class GatherModule
+    {
+    }
+}
